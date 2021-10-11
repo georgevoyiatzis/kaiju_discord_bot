a2951c742fe3ca997a7bc3393c316dd9cd1c7947
georgevoyiatzis <rz2345@DESKTOP-P9JPBI2.localdomain>
2021-10-11 03:57:55 -0500

    Bamboozle on !whitelist

--- bot.py
+++ bot.py
@@ -7,4 +7,9 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 
 client = discord.Client()
 
+@client.event
+async def on_message(message):
+    if message.content.split(" ")[0] == "!whitelist":
+        await message.channel.send(':clown:')
+
 client.run(TOKEN)
