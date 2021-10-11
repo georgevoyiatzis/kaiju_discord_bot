e1fc926b596fff69d0ec56cffae2969ab2a66098
georgevoyiatzis <rz2345@DESKTOP-P9JPBI2.localdomain>
2021-10-11 05:35:41 -0500

    Links and Announcements

--- bot.py
+++ bot.py
@@ -7,6 +7,8 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 
 client = discord.Client()
 
+announcements_ch_id = 892056910356946954 #announcements ch_id
+
 @client.event
 async def on_message(message):
     if message.content.split(" ")[0] == "!whitelist":
@@ -15,5 +17,11 @@ async def on_message(message):
         await message.channel.send('ROAAAAAAARRRR')
     elif message.content.split(' ')[0] == "!links":
         await message.channel.send("Official site: https://kaijukingz.io/#/ \nOfficial OpenSea Collection: https://opensea.io/collection/kaiju-kingz \nOfficial Contract: https://etherscan.io/address/0x1685133a98e1d4fc1fe8e25b7493d186c37b6b24")
+    elif message.content.split(" ")[0] == "!opensea":
+        await message.channel.send("Official OpenSea Collection: https://opensea.io/collection/kaiju-kingz")
+    elif message.content.split(" ")[0] == "!wensale":
+        guild = message.guild
+        announcements_ch = guild.get_channel(announcements_ch_id)
+        await message.channel.send(f"Please refer to {announcements_ch.mention}!")
 
 client.run(TOKEN)
